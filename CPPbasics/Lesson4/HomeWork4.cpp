4057207c7f1b4b5ccff8459e8da0902c9a05ba85
Vlad3955 <vladreut28@gmail.com>
2021-10-06 19:16:53 +0300

    add Homework4.cpp

---
+++ CPPbasics/Lesson4/HomeWork4.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+//============================================================
+//Task 1
+//============================================================
+
+void task1()
+{
+	int num;
+	cout << "Введите число: " << endl;
+	cin >> num;
+	if (num >= 10 && num <= 20) 
+	{
+		cout << "true" << endl;
+	} 
+	else
+	{
+		cout << "false" << endl;
+	}
+}
+
+
+//============================================================
+//Task 2
+//============================================================
+
+void task2()
+{
+	const int NUM1 = 10, NUM2 = 10;
+	
+	
+	if (NUM1 == 10 && NUM2 == 10 || NUM1 + NUM2 == 10)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+}
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	//task1();
+	task2();
+
+
+	return 0;
+}
