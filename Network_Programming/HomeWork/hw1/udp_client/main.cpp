590fd0ab3071b702e9fd1ecbcb23dc637b2da48a
Vlad3955 <vladreut28@gmail.com>
2022-10-13 12:44:21 +0300

    some mod to HW1 to HomeWork/CPP/CPP

--- Network_Programming/HomeWork/hw1/udp_client/main.cpp
+++ Network_Programming/HomeWork/hw1/udp_client/main.cpp
@@ -1,6 +1,15 @@
-#include "uclient.h"
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <cstring>
 
-#define SERVER "localhost"  // or "localhost" - ip address of UDP server
+#include <socket_wrapper/socket_headers.h>
+#include <socket_wrapper/socket_wrapper.h>
+#include <socket_wrapper/socket_class.h>
+
 #define BUFLEN 512  // max length of answer
 
 
@@ -20,7 +29,7 @@ int main(int argc, char const* argv[])
 
     socket_wrapper::Socket sock = { AF_INET, SOCK_DGRAM, IPPROTO_UDP };
 
-    std::cout << "Starting echo server on the port " << port << "...\n";
+    std::cout << "Starting echo client on the port " << port << "...\n";
 
     if (!sock)
     {
@@ -31,63 +40,12 @@ int main(int argc, char const* argv[])
     memset((char*)&server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
-    server.sin_addr.s_addr = inet_addr(SERVER);
-
+    inet_pton(AF_INET, "192.168.4.73", &server.sin_addr);
     while (true)
     {
-        //// Read content into buffer from an incoming client.
-        //recv_len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0,
-        //    reinterpret_cast<sockaddr*>(&client_address),
-        //    &client_address_len);
-
-
-
-        //int x = 0;
-        //if (recv_len > 0)
-        //{
-        //    buffer[recv_len] = '\0';
-        //    std::cout
-        //        << "Client with address: "
-        //        << inet_ntop(AF_INET, &client_address.sin_addr, client_address_buf, sizeof(client_address_buf) / sizeof(client_address_buf[0]))
-        //        << ":" << ntohs(client_address.sin_port)
-        //        << " and host name: ";
-        //    if (x = getnameinfo(reinterpret_cast<sockaddr*>(&client_address), client_address_len,
-        //        hbuf, sizeof(hbuf), nullptr, 0, NI_NAMEREQD) != 0)
-        //    {
-        //        std::cerr << "could not resolve hostname" << std::endl;
-        //    }
-        //    else
-        //    {
-        //        std::cout << "host" << hbuf << std::endl;
-        //    }
-        //    std::cout << " sent datagram "
-        //        << "[length = "
-        //        << recv_len
-        //        << "]:\n'''\n"
-        //        << buffer
-        //        << "\n'''"
-        //        << std::endl;
-        //    std::string command_string = { buffer, 0, len };
-        //    rtrim(command_string);
-        //    if ("exit" == command_string)
-        //    {
-        //        std::cout << "See you!" << std::endl;
-        //        run = false;
-        //    }
-
-        //    // send(sock, &buf, readden, 0);
-
-        //    // Send same content back to the client ("echo").
-        //    sendto(sock, buffer, recv_len, 0, reinterpret_cast<const sockaddr*>(&client_address),
-        //        client_address_len);
-        //}
-
-
         char message[BUFLEN];
         printf("Enter message: ");
-        //std::cin.getline(message, BUFLEN);
-        //std::cout << "Enter message: " << std::endl;
-        std::cin >> message;
+        std::cin.getline(message, BUFLEN);
 
         // send the message
         sendto(sock, message, strlen(message), 0, (sockaddr*)&server, sizeof(sockaddr_in));
@@ -110,12 +68,6 @@ int main(int argc, char const* argv[])
 
         std::cout << std::endl;
     }
-
-
-    
-    /*UDPClient udpclient;
-    
-    udpclient.SendUDP(sock);*/
 
     return EXIT_SUCCESS;
 }
